195b17cd0bf8a9665ad43ea9fdd0e999779b4acd
Chris St. Jean <chris.r.stjean@gmail.com>
2024-10-12 12:09:10 -0700

    adding loggint to figure out wtf is happening

--- activity-worker/helpers/getCloudSqlConnection.ts
+++ activity-worker/helpers/getCloudSqlConnection.ts
@@ -1,12 +1,13 @@
 import { Connector, IpAddressTypes } from "@google-cloud/cloud-sql-connector";
-import mysql, { Connection } from "mysql2/promise";
+import mysql, { Connection, PoolConnection } from "mysql2/promise";
 import storage from "node-persist";
 
 const connector = new Connector();
 
 const getCloudSqlConnection = async () => {
+    console.log("Getting cloud SQL connection");
     await storage.init();
-    const cachedConnection: Connection = await storage.getItem(
+    const cachedConnection: PoolConnection = await storage.getItem(
         "cloudSqlConnection"
     );
 
@@ -14,19 +15,31 @@ const getCloudSqlConnection = async () => {
         console.log("Using cached connection");
         return cachedConnection;
     } else {
+        console.log("Creating new connection");
         const clientOpts = await connector.getOptions({
             instanceConnectionName: process.env.INSTANCE_CONNECTION_NAME ?? "",
             ipType: IpAddressTypes.PUBLIC,
         });
 
-        const connection = await mysql.createConnection({
+        console.log("Retrieved client options");
+
+        const pool = await mysql.createPool({
             ...clientOpts,
             user: "local-user",
             password: process.env.MYSQL_PASSWORD,
             database: "dev-db",
         });
 
-        storage.setItem("cloudSqlConnection", connection);
+        console.log("Created pool");
+
+        const connection = await pool.getConnection();
+
+        console.log("Created connection");
+
+        if (process.env.NODE_ENV === "production")
+            storage.setItem("cloudSqlConnection", connection);
+
+        console.log("Stored connection");
 
         return connection;
     }
