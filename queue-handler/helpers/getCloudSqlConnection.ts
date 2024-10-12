1bbd0161f735d91a3e3e26b8fbcdb6237e686b18
Chris St. Jean <chris.r.stjean@gmail.com>
2024-10-11 17:40:39 -0700

    testing db connection

--- queue-handler/helpers/getCloudSqlConnection.ts
+++ queue-handler/helpers/getCloudSqlConnection.ts
@@ -1,8 +1,9 @@
 import { Connector, IpAddressTypes } from "@google-cloud/cloud-sql-connector";
 import mysql from "mysql2/promise";
 
+const connector = new Connector();
+
 const getCloudSqlConnection = async () => {
-    const connector = new Connector();
     const clientOpts = await connector.getOptions({
         instanceConnectionName: process.env.INSTANCE_CONNECTION_NAME ?? "",
         ipType: IpAddressTypes.PUBLIC,
--- queue-handler/helpers/processMessages.ts
+++ queue-handler/helpers/processMessages.ts
@@ -6,45 +6,77 @@ import getNumberOfMessages from "./getNumberOfMessages";
 import resetShortTermUsage from "./resetShortTermUsage";
 import retrieveMessage from "./retrieveMessage";
 import QueueMessage from "../typeDefs/QueueMessage";
+import getMessagesToProcess from "./getMessagestoProcess";
+import { PubSub } from "@google-cloud/pubsub";
+import completeMessage from "./completeMessage";
+
+const topicName = process.env.PUBSUB_TOPIC ?? "";
+
+const pubSubClient = new PubSub();
 
 const processMessages = async () => {
-    console.log("processing messages");
-
     const connection = await getCloudSqlConnection();
 
-    await resetShortTermUsage(connection);
+    const messages = await getMessagesToProcess(connection);
 
-    const allowedProcessing = await checkRateLimit(connection, false);
+    messages.forEach(async (message) => {
+        const data = JSON.stringify(message);
+        const dataBuffer = Buffer.from(data);
 
-    console.log(`Allowed processing: ${allowedProcessing}`);
-
-    const numberOfMessages = await getNumberOfMessages(connection);
-
-    const messagesToProcess = Math.min(allowedProcessing, numberOfMessages);
-
-    if (messagesToProcess === 0) {
-        console.log("No messages to process");
-        return;
-    } else {
-        console.log(`Processing ${messagesToProcess} messages`);
-
-        const queue = async.queue(async (message: QueueMessage, callback) => {
-            const success = await retrieveMessage(connection, message);
-            callback();
-        }, 20);
-
-        for (let i = 0; i < messagesToProcess; i++) {
-            await getMostRecentMessage(connection, (message) => {
-                queue.push(message, () => {
-                    console.log(`Message ${message.id} processed`);
-                });
-            });
+        try {
+            const messageId = await pubSubClient
+                .topic(topicName)
+                .publishMessage({ data: dataBuffer });
+            console.log(`Message ${messageId} published.`);
+        } catch (error) {
+            console.error(
+                `Received error while publishing: ${(error as Error).message}`
+            );
+            await completeMessage(
+                connection,
+                message.id,
+                (error as Error).message
+            );
+            process.exitCode = 1;
         }
+    });
+    // console.log("processing messages");
 
-        await queue.drain();
+    // const connection = await getCloudSqlConnection();
 
-        console.log("No more messages to process");
-    }
+    // await resetShortTermUsage(connection);
+
+    // const allowedProcessing = await checkRateLimit(connection, false);
+
+    // console.log(`Allowed processing: ${allowedProcessing}`);
+
+    // const numberOfMessages = await getNumberOfMessages(connection);
+
+    // const messagesToProcess = Math.min(allowedProcessing, numberOfMessages);
+
+    // if (messagesToProcess === 0) {
+    //     console.log("No messages to process");
+    //     return;
+    // } else {
+    //     console.log(`Processing ${messagesToProcess} messages`);
+
+    //     const queue = async.queue(async (message: QueueMessage, callback) => {
+    //         const success = await retrieveMessage(connection, message);
+    //         callback();
+    //     }, 20);
+
+    //     for (let i = 0; i < messagesToProcess; i++) {
+    //         await getMostRecentMessage(connection, (message) => {
+    //             queue.push(message, () => {
+    //                 console.log(`Message ${message.id} processed`);
+    //             });
+    //         });
+    //     }
+
+    //     await queue.drain();
+
+    //     console.log("No more messages to process");
+    // }
 };
 
 export default processMessages;
