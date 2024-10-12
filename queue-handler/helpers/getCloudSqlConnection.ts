ea6ac863d78c5147183485f2cc4b7a135ffde5bc
Chris St. Jean <chris.r.stjean@gmail.com>
2024-10-12 12:45:49 -0700

    bug fixing cache

--- queue-handler/helpers/getCloudSqlConnection.ts
+++ queue-handler/helpers/getCloudSqlConnection.ts
@@ -7,58 +7,57 @@ const connector = new Connector();
 const getCloudSqlConnection = async () => {
     console.log("Getting cloud SQL connection");
     // await storage.init();
-    const cachedConnection: PoolConnection = await storage.getItem(
-        "cloudSqlConnection"
-    );
+    // const cachedConnection: PoolConnection = await storage.getItem(
+    //     "cloudSqlConnection"
+    // );
 
-    if (cachedConnection) {
-        console.log("Using cached connection");
-        return cachedConnection;
+    // if (cachedConnection) {
+    //     console.log("Using cached connection");
+    //     return cachedConnection;
+    // } else {
+    console.log("Retrieved client options");
+
+    if (process.env.NODE_ENV === "production") {
+        const pool = await mysql.createPool({
+            user: "local-user",
+            password: process.env.MYSQL_PASSWORD,
+            database: "dev-db",
+            socketPath: "/cloudsql/" + process.env.INSTANCE_CONNECTION_NAME,
+        });
+
+        console.log("Created pool");
+
+        const connection = await pool.getConnection();
+
+        console.log("Created connection");
+
+        // storage.setItem("cloudSqlConnection", connection);
+
+        // console.log("Stored connection");
+
+        return connection;
     } else {
-        console.log("Retrieved client options");
+        const clientOpts = await connector.getOptions({
+            instanceConnectionName: process.env.INSTANCE_CONNECTION_NAME ?? "",
+            ipType: IpAddressTypes.PUBLIC,
+        });
 
-        if (process.env.NODE_ENV === "production") {
-            const pool = await mysql.createPool({
-                user: "local-user",
-                password: process.env.MYSQL_PASSWORD,
-                database: "dev-db",
-                socketPath: "/cloudsql/" + process.env.INSTANCE_CONNECTION_NAME,
-            });
+        const pool = await mysql.createPool({
+            user: "local-user",
+            password: process.env.MYSQL_PASSWORD,
+            database: "dev-db",
+            ...clientOpts,
+        });
 
-            console.log("Created pool");
+        console.log("Created pool");
 
-            const connection = await pool.getConnection();
+        const connection = await pool.getConnection();
 
-            console.log("Created connection");
+        console.log("Created connection");
 
-            // storage.setItem("cloudSqlConnection", connection);
-
-            // console.log("Stored connection");
-
-            return connection;
-        } else {
-            const clientOpts = await connector.getOptions({
-                instanceConnectionName:
-                    process.env.INSTANCE_CONNECTION_NAME ?? "",
-                ipType: IpAddressTypes.PUBLIC,
-            });
-
-            const pool = await mysql.createPool({
-                user: "local-user",
-                password: process.env.MYSQL_PASSWORD,
-                database: "dev-db",
-                ...clientOpts,
-            });
-
-            console.log("Created pool");
-
-            const connection = await pool.getConnection();
-
-            console.log("Created connection");
-
-            return connection;
-        }
+        return connection;
     }
+    // }
 };
 
 export default getCloudSqlConnection;
