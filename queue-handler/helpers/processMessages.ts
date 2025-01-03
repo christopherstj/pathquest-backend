1304e246cfd303bd5349a2ed6e9521d17bd4d2cb
Chris St. Jean <chris.r.stjean@gmail.com>
2025-01-03 12:33:40 -0700

    ending connection

--- queue-handler/helpers/processMessages.ts
+++ queue-handler/helpers/processMessages.ts
@@ -33,43 +33,8 @@ const processMessages = async () => {
             process.exitCode = 1;
         }
     });
-    // console.log("processing messages");
 
-    // const connection = await getCloudSqlConnection();
-
-    // await resetShortTermUsage(connection);
-
-    // const allowedProcessing = await checkRateLimit(connection, false);
-
-    // console.log(`Allowed processing: ${allowedProcessing}`);
-
-    // const numberOfMessages = await getNumberOfMessages(connection);
-
-    // const messagesToProcess = Math.min(allowedProcessing, numberOfMessages);
-
-    // if (messagesToProcess === 0) {
-    //     console.log("No messages to process");
-    //     return;
-    // } else {
-    //     console.log(`Processing ${messagesToProcess} messages`);
-
-    //     const queue = async.queue(async (message: QueueMessage, callback) => {
-    //         const success = await retrieveMessage(connection, message);
-    //         callback();
-    //     }, 20);
-
-    //     for (let i = 0; i < messagesToProcess; i++) {
-    //         await getMostRecentMessage(connection, (message) => {
-    //             queue.push(message, () => {
-    //                 console.log(`Message ${message.id} processed`);
-    //             });
-    //         });
-    //     }
-
-    //     await queue.drain();
-
-    //     console.log("No more messages to process");
-    // }
+    await connection.end();
 };
 
 export default processMessages;
